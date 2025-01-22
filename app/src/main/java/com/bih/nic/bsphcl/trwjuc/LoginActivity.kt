0d02d3d2ef08080b67ad21e7e660c03baa848a29
chandan <chandan101288@gmail.com>
2025-01-22 15:54:23 +0530

    initial commit

---
+++ app/src/main/java/com/bih/nic/bsphcl/trwjuc/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.bih.nic.bsphcl.trwjuc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
