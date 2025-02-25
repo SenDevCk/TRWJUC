fa7833a6d436378234f11101b036cbc4e73d1970
chandan <chandan101288@gmail.com>
2025-02-25 13:15:18 +0530

    nineteenth commit3

--- app/src/main/java/com/bih/nic/bsphcl/trwjuc/retrofit/RetrofitHelperWithJwt.kt
+++ app/src/main/java/com/bih/nic/bsphcl/trwjuc/retrofit/RetrofitHelperWithJwt.kt
@@ -1,5 +1,8 @@
 package com.bih.nic.bsphcl.trwjuc.retrofit
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,11 +13,31 @@ object RetrofitHelperWithJwt {
 
 
 
-    fun getInstance(): Retrofit {
-        return Retrofit.Builder().baseUrl(Urls.baseUrl)
+    fun getInstance(jwtToken: String): Retrofit {
+        // Create an Interceptor to add the JWT token to headers
+        val interceptor = Interceptor { chain ->
+            // Get the request
+            val originalRequest: Request = chain.request()
+
+            // Add Authorization header with JWT token
+            val newRequest = originalRequest.newBuilder()
+                .header("Authorization", "Bearer $jwtToken")
+                .build()
+
+            // Proceed with the request
+            chain.proceed(newRequest)
+        }
+
+        // Create an OkHttpClient and add the interceptor
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        // Return Retrofit instance
+        return Retrofit.Builder()
+            .baseUrl(Urls.baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
-            // we need to add converter factory to
-            // convert JSON object to Java object
+            .client(client) // Pass the OkHttpClient with JWT Interceptor
             .build()
     }
 }
